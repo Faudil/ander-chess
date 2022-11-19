70e7494e0ff0c9ec8ece0e2ac81a2fbd67050005
Faudil Puttilli <faudil.puttilli@gmail.com>
2022-11-19 19:36:52 +0100

    minmax is now negamax and a bit less dumb

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use rayon::prelude::*;
 use std::{thread, time};
 
 use std::cmp::{max, min};
-use std::io::stdin;
+use std::io::{stdin, stdout, Write};
 use std::ops::Index;
 use std::thread::scope;
 use pleco::board::Board;
@@ -17,7 +17,7 @@ use pleco::{PieceType, Player};
 use rand::seq::SliceRandom; // 0.7.2
 
 fn count_pieces_sore(board: &Board, player: Player) -> i8 {
-    let mut score = board.count_piece(player, PieceType::P);
+    let mut score = 0;
     score += board.count_piece(player, PieceType::P) * 1;
     score += board.count_piece(player, PieceType::B) * 3;
     score += board.count_piece(player, PieceType::N) * 3;
@@ -33,7 +33,7 @@ fn count_score(board: &Board, player: Player) -> i8 {
 
 fn compute_move(mut board: Board, player: Player, chess_move: BitMove) -> i8 {
     board.apply_move(chess_move);
-    let score = minmax(&mut board, 3, -99, 99, false, player);
+    let score = -negamax(&mut board, 3, -120, 120, player.other_player());
     board.undo_move();
     score
 }
@@ -44,47 +44,32 @@ fn process(mut board: Board, depth: i8, player: Player) -> BitMove {
     let scores: Vec<i8> = moves.par_iter().map(|chess_move| compute_move(board.clone(), player, *chess_move)).collect();
     let max_value = scores.iter().max().unwrap();
     let max_idx = scores.iter().position(|x| x == max_value).unwrap();
+    for i in 0..moves.len() {
+        print!("{}:{} ", moves[i], scores[i])
+    }
+    println!();
     return moves[max_idx];
 }
 
-fn minmax(board: & mut Board, depth: i8, mut alpha: i8, mut beta: i8, maximising: bool, player: Player) -> i8 {
+fn negamax(board: &mut Board, depth: i8, mut alpha: i8, mut beta: i8, player: Player) -> i8 {
     if depth == 0 {
-        return count_score(board, player);
+        return if board.turn() == player {
+            count_score(board, player)
+        } else {
+            -count_score(board, player)
+        }
     }
-    let mut value = 0;
-    // max
-    if maximising {
-        value = -99;
-        let moves = board.generate_moves();
-        for chess_move in moves {
-            board.apply_move(chess_move);
-            let leaf_value = minmax(board, depth - 1, alpha, beta, !maximising, player);
-            if value < leaf_value {
-                value = leaf_value;
-            }
-            if value >= beta {
-                board.undo_move();
-                break;
-            }
-            alpha = min(alpha, value);
+    let mut value = -120;
+    let moves = board.generate_moves();
+    for chess_move in moves {
+        board.apply_move(chess_move);
+        value = max(value, -negamax(board, depth - 1, -beta, -alpha, player.other_player()));
+        alpha = max(alpha, value);
+        if alpha >= beta {
             board.undo_move();
+            break;
         }
-    } else {
-        value = 99;
-        let moves = board.generate_moves();
-        for chess_move in moves {
-            board.apply_move(chess_move);
-            let leaf_value = minmax(board, depth - 1, alpha, beta, !maximising, player);
-            if value > leaf_value {
-                value = leaf_value;
-            }
-            if value <= alpha {
-                board.undo_move();
-                break;
-            }
-            beta = max(beta, value);
-            board.undo_move();
-        }
+        board.undo_move();
     }
     return value;
 }
@@ -95,13 +80,20 @@ fn play_game() {
     let mut s= String::new();
     board.pretty_print();
     while !board.checkmate() || !board.stalemate() {
-        stdin().read_line(&mut s).expect("Did not enter a correct string");
-        s.pop();
-        // board.apply_uci_move(s.as_str());
-        let first_player_move = process(board.clone(), 2, board.turn());
-        board.apply_move(first_player_move);
+        let mut applied = false;
+        while !applied {
+            print!("Please enter your move: ");
+            stdout().flush();
+            stdin().read_line(&mut s).expect("Did not enter a correct string");
+            s.pop();
+            println!("{}", s.as_str());
+            applied = board.apply_uci_move(s.as_str());
+            s = String::new();
+        }
+        // let first_player_move = process(board.clone(), 2, board.turn());
+        // board.apply_move(first_player_move);
         board.pretty_print();
-        stdin().read_line(&mut s).expect("Did not enter a correct string");
+        // stdin().read_line(&mut s).expect("Did not enter a correct string");
         let engine_move = process(board.clone(), 2, board.turn());
         board.apply_move(engine_move);
         board.pretty_print();
